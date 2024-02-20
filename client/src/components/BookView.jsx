4ae96ee234f758a0af48c31a40027b1849f6d876
SrizaBiswas <biswassriza76@gmail.com>
2024-02-20 11:58:29 +0530

    first

---
+++ client/src/components/BookView.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const BookView = () => {
+  return (
+    <aside className=" w-25 h-vh bg-white text-black">
+      <div>chp1</div>
+      <div>chp1</div>
+      <div>chp1</div>
+    </aside>
+    // <div className="">
+    // <div>
+    //     njll
+    // </div>
+    // </div>
+  );
+};
+
+export default BookView;
